79059cc678a195f1347140be19ac7e20ba958b98
BytemanD <byteman.e@outlook.com>
2026-03-26 23:45:19 +0800

    feat: 使用 pydantic 计算时间范围
    Signed-off-by: BytemanD <byteman.e@outlook.com>

--- src/git_stats/main.py
+++ src/git_stats/main.py
@@ -1,13 +1,41 @@
-from typing import Tuple
+from datetime import datetime
+from typing import Set, Tuple
 
 import click
 from git import Repo
+from pystonic.utils import dateutil
 from rich import box
 from rich.console import Console
-from rich.table import Table
+from rich.table import Column, Table
 from rich.text import Text
 
 console = Console()
+
+
+def parse_date_range(date_range: Set[str]) -> Tuple[datetime, datetime]:
+    """Parse date range"""
+    if not date_range:
+        return dateutil.thisday()
+    if len(date_range) == 1:
+        if date_range[0] in ["today", "thisday"]:
+            return dateutil.thisday()
+        if date_range[0] == "yesterday":
+            return dateutil.yestoday()
+        if date_range[0] == "thisweek":
+            return dateutil.thisweek()
+        if date_range[0] == "lastweek":
+            return dateutil.lastweek()
+        if date_range[0] == "thismonth":
+            return dateutil.thismonth()
+        if date_range[0] == "lastmonth":
+            return dateutil.lastmonth()
+        return datetime.strptime(date_range[0], dateutil.FORMAT_DATETIME)
+    elif len(date_range) == 2:
+        return datetime.strptime(
+            date_range[0], dateutil.FORMAT_DATETIME
+        ), datetime.strptime(date_range[1], dateutil.FORMAT_DATETIME)
+
+    raise ValueError("Invalid date range")
 
 
 @click.group()
@@ -16,31 +44,53 @@ def app():
 
 
 @app.command()
-def lines():
-    """Count lines"""
+@click.argument("date_range", nargs=-1, default=[], required=False)
+def lines(date_range: Set[str]):
+    """Count lines
+
+    \b
+    e.g.
+        git-stats lines
+        git-stats lines <today | yesterday | thisweek | thismonth>
+        git-stats lines 2026-01-12 2026-01-22
+        ...
+    """
+    try:
+        since, until = parse_date_range(date_range)
+    except ValueError:
+        raise click.BadParameter("parse date range error")
+
     repo = Repo()
-    commits_total: dict[str, Tuple[int, int, int]] = {}
-    for commit in repo.iter_commits():
-        author = commit.author.email
+    commits_total: dict[str, Tuple[int, int, int, int]] = {}
+    for commit in repo.iter_commits(since=since, until=until):
+        author = commit.author.name or commit.author.email or "Unknown"
         total = commit.stats.total
 
-        commits_total.setdefault(author, [0, 0, 0])
+        commits_total.setdefault(author, [0, 0, 0, 0])
         commits_total[author][0] += total.get("insertions", 0)
         commits_total[author][1] += total.get("deletions", 0)
         commits_total[author][2] += total.get("lines", 0)
+        commits_total[author][3] += 1
+
+    console.print(f"{since:%Y-%m-%d %H:%M:%S} ~ {until:%Y-%m-%d %H:%M:%S}", style="cyan underline")
+    click.secho()
 
     table = Table(
-        "Author", header_style="bold cyan", title="Commit lines", box=box.SIMPLE
+        Column("Author"),
+        Column("Added", justify="right"),
+        Column("Removed", justify="right"),
+        Column("Total", justify="right"),
+        Column("Commits", justify="right"),
+        title="Code lines",
+        box=box.SIMPLE,
     )
-    table.add_column("Added", justify="right")
-    table.add_column("Removed", justify="right")
-    table.add_column("Total", justify="right")
     for author, commit in commits_total.items():
         table.add_row(
             author,
             Text(str(commit[0]), style="green"),
             Text(str(commit[1]), style="red"),
             str(commit[2]),
+            str(commit[3]),
         )
 
     console.print(table)
